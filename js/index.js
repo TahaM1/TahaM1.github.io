b0845d7e7d771865958847bde4ec2e46080d0f2e
Taha <tahanaeem12@hotmail.com>
2020-09-13 00:33:12 -0400

    nav menu hides after clicking link

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 const navToggle = document.querySelector(".toggle-nav");
+const navLinkList = document.querySelectorAll(".nav__link");
 
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-clicked");
+});
+
+navLinkList.forEach((element) => {
+  element.addEventListener("click", () => {
+    document.body.classList.remove("nav-clicked");
+  });
 });
